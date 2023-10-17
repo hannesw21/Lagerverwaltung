d9b9d9ac264bb1699a57377c7b64cb08e890c122
student <hannes.weber2001@gmail.com>
2023-10-17 08:22:20 +0200

    Init

---
+++ src/de/sbs/fswi2/MainView.java
@@ -0,0 +1,14 @@
+package de.sbs.fswi2;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainView extends Application {
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.show();
+    }
+}
