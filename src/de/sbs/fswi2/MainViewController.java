87066022f7250b80e33cf331f337bc04759d88ec
student <hannes.weber2001@gmail.com>
2023-11-14 09:14:35 +0100

    Update

--- src/de/sbs/fswi2/MainViewController.java
+++ src/de/sbs/fswi2/MainViewController.java
@@ -8,6 +8,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 public class MainViewController implements Initializable {
 	@FXML
@@ -20,9 +21,17 @@ public class MainViewController implements Initializable {
 
     @FXML
     private Label lblDatum;
+    private Stage primaryStage;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {  
         lblDatum.setText(new java.text.SimpleDateFormat("dd.MM.yyyy").format(new java.util.Date())); 
+        System.out.println(daoservice.DAOHttpService.useService());
     }
+    public void setStage (Stage primaryStage){
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Beispielfenster");
+    }
+
+
 }
